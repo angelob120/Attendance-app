3cca06f30baae09335988127220e44d549ee8576
Angelo Brown <130711987+angelob120@users.noreply.github.com>
2024-09-11 08:41:44 -0400

    23e

---
+++ Attendance-app/Attendance-app/Attendance_appApp.swift
@@ -0,0 +1,32 @@
+//
+//  Attendance_appApp.swift
+//  Attendance-app
+//
+//  Created by AB on 9/11/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Attendance_appApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
